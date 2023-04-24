07258adaa23f706e81329b11f253682795db703f
Ilya Bondarchuk <cod3provider@gmail.com>
2023-04-24 21:12:55 +0300

    add started files

--- controllers/contacts/add.js
+++
@@ -1,8 +0,0 @@
-const {Contact} = require('@root/models');
-
-const add = async (req, res) => {
-	const result = await Contact.create(req.body);
-	res.status(201).json(result);
-};
-
-module.exports = add;
